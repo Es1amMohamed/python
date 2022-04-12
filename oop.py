7dc68e456d9af80d6c52294979f6a597c60f3f3e
Es1amMohamed <EslamMohamedMetwally1@gmail.com>
2022-04-13 00:14:04 +0200

    'make_review'

---
+++ oop.py
@@ -0,0 +1,31 @@
+
+class Games:
+        def __init__(self ):
+            pass
+            
+            
+        def one_game(self, onegame):
+            
+            print(f'I Have One Game Called "{onegame}"')
+            
+        def allgames(self):
+            # I Have Many Games:
+            # -- Ys II
+            # -- Ys Oath In Felghana
+            # -- YS Origin
+            pass
+            
+        def numgames(self):
+            print('num')
+            
+s = Games()
+
+s.one_game("lol")
+
+        
+
+
+
+
+
+
