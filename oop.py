a49e8920e77f3c09887b81d3ed0fa813c50fb04c
Es1amMohamed <EslamMohamedMetwally1@gmail.com>
2022-04-18 00:33:02 +0200

    end the task

--- oop.py
+++ oop.py
@@ -6,21 +6,47 @@ class Games:
             
         def one_game(self, onegame):
             
-            print(f'I Have One Game Called "{onegame}"')
+            return f'I Have One Game Called "{onegame}"'
             
-        def allgames(self):
-            # I Have Many Games:
-            # -- Ys II
-            # -- Ys Oath In Felghana
-            # -- YS Origin
-            pass
-            
-        def numgames(self):
-            print('num')
-            
-s = Games()
+        def allgames(self,game):
+            self.games = []
+            print(e)
+            print('I Have Many Games:')
+            for i in e :
+                 print(f'-{i} ')
+                 
+        def numgames(self, num):
+            self.num = num
+            return f'You Have {num} Games '
 
-s.one_game("lol")
+question = input('What is games do you play? ')
+question2 = input('Do You Have Another Games ? y/n ').lower()
+s = Games()
+e = []
+e.append(question)
+
+if question in '0123456789' :
+    print(s.numgames(question))
+    
+elif question2.lower() in 'no' and question2 != '0123456789':
+    print(s.one_game(question))
+    
+elif question2.lower() in 'yes':
+    while True :
+        question3 = input('What is games do you play? ')
+        e.append(question3)
+        question4 = input('Do You Have Another Games ? y/n ').lower()
+        if question4.lower() in 'no':
+            print(s.allgames(e))
+            break
+            
+        
+        
+        
+        
+    
+    
+    
 
         
 
